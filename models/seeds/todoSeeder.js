ec325d90cc2427095df0d674f8cf0a56c704b0e2
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-01 22:42:48 +0800

    feat: add todo seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,7 +1,5 @@
-// 載入 express 並建構應用程式伺服器
-const express = require('express')
 const mongoose = require('mongoose') //載入mongoose
-const app = express()
+const Todo = require('../todo') //載入todo model
 
 //設定連線到 mongoDB，在連線資料庫的同時傳入設定，可以直接把兩組設定合併成一個物件，更新語法
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,15 +15,8 @@ db.on('error', () => {
 //連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
-})
-
-
-//設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-//設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
+  }
+  console.log('done')
 })
