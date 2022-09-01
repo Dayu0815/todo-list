66aee8f1fb106125029c5269e93a7fb740db7660
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-01 21:00:02 +0800

    0901日連線資料庫並建立 Todo Model

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') //載入mongoose
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI) //設定連線到 mongoDB
+
+//取得資料連線狀態
+const db = mongoose.connection
+
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 //設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world！')
+  res.send('hello world')
 })
 
 //設定 port 3000
