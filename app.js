45cf2977a63a8d2f549ac256207eda3aa2540113
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-04 10:31:59 +0800

    feat: modify HTTP 請求方法 (Request Method)

--- app.js
+++ app.js
@@ -36,60 +36,60 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) //如果發生意外，執行錯誤處理
 })
 
-// 設定路由 get 讀取 New 頁面資料 
+// 設定路由 get 列出 New 頁面全部資料_ Read  
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
-// 設定路由 post 接住表單資料，送往資料庫新增_ 完成 Create 動作
+// 設定路由 post 接住表單資料，送往資料庫新增_ Create
 app.post('/todos', (req, res) => {
-  const name = req.body.name       //從 req.body 取出表單裡的 name 資料
-  return Todo.create({ name })     //存入資料庫，完成 create 動作
-    .then(() => res.redirect('/')) //完成 create 動作後，返回首頁
-    .catch(error => console.log(error))
+  const name = req.body.name            //從 req.body 取出表單裡的 name 資料
+  return Todo.create({ name })          //存入資料庫，完成 create 動作
+    .then(() => res.redirect('/'))      //完成 create 動作後，返回首頁
+    .catch(error => console.log(error)) //如果發生意外，執行錯誤處理
 })
 
-// 設定路由 get 列出某一筆特定 To-do
+// 設定路由 get 讀取查詢資料庫，列出某一筆特定資料_ Read
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findById(id)         //從資料庫查詢找出資料
-    .lean()                        //把資料轉換成，乾淨單純的 JS 物件
+  return Todo.findById(id)               //從資料庫查詢找出資料
+    .lean()                              //把資料轉換成，乾淨單純的 JS 物件
     .then((todo) => res.render('detail', { todo })) //把資料送給前端 detail 樣版
     .catch(error => console.log(error))  //如果發生意外，執行錯誤處理
 })
 
-// 設定路由 get 修改特定 To-do
+// 設定路由 get 讀取查詢資料庫，修改特定資料_ Update
 app.get('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  return Todo.findById(id)         //從資料庫查詢找出資料
-    .lean()                        //把資料轉換成，乾淨單純的 JS 物件
+  return Todo.findById(id)               //從資料庫查詢找出資料
+    .lean()                              //把資料轉換成，乾淨單純的 JS 物件
     .then((todo) => res.render('edit', { todo })) //把資料送給前端 edit 樣版
     .catch(error => console.log(error))  //如果發生意外，執行錯誤處理
 })
 
-// 設定路由 post 修改特定 To-do
+// 設定路由 post 讀取查詢資料庫，接住修改後資料，送往資料庫儲存_ Update
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
   const name = req.body.name
-  return Todo.findById(id)            //從資料庫查詢找出資料
-    .then(todo => {                  //如果查詢成功，把資料修改後，重新儲存資料
+  return Todo.findById(id)               //從資料庫查詢找出資料
+    .then(todo => {                      //如果查詢成功，把資料修改後，重新儲存資料
       todo.name = name
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`)) //如果儲存成功，返回首頁
-    .catch(error => console.log(error))
+    .catch(error => console.log(error))  //如果發生意外，執行錯誤處理
 })
 
-//設定路由 post 刪除特定 To-do
+//設定路由 post 讀取查詢資料庫，刪除特定資料_ Delete
 app.post('/todos/:id/delete', (req, res) => {
   const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+  return Todo.findById(id)                //從資料庫查詢找出資料
+    .then(todo => todo.remove())          //如果查詢成功，把資料刪除
+    .then(() => res.redirect('/'))        //完成 delete 動作後，返回首頁
+    .catch(error => console.log(error))   //如果發生意外，執行錯誤處理
 })
 
-//設定 port 3000
+//設定監聽 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
