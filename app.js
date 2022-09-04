46cd8d573d7137e8b29a87a1b074e613b3cc1634
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-04 22:10:41 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -18,13 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //取得資料連線狀態
 const db = mongoose.connection
-
-//連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-
-//連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
