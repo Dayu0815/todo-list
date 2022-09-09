4c0fa29f3c6687c00b9ce22a260176a4a23f09fd
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-09 14:42:30 +0800

    feat: modify models/todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+
+// 定義資料結構 Schema（資料庫綱要）
+// 用 JSON 的方式來告訴 mongo 說 document 的資料會包含哪些型態
+// findById() 找的是 _id，不是.id，_id 屬性不需要在 Schema 裡指定，Mongoose 會自動產生
 const Schema = mongoose.Schema
 const todoSchema = new Schema({
   name: {
