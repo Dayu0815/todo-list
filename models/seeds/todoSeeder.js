c86723f1c233f1483cc25ca98c5c3a0ad792256d
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-09 15:53:03 +0800

    feat: model todo add isDone params

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -6,15 +6,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 //取得資料連線狀態
 const db = mongoose.connection
-
-//連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-
-//連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
+
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
