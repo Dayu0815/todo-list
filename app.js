ef86f38f89e0f6c698d4bdb1668eb90475a00e29
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-09 16:17:59 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find()                             //取出 todo model 所有資料
     .lean()                               // 把 Model 物件，轉換成乾淨單純的 JS 資料陣列
+    .sort({ _id: 'asc' })                 // 根據 _id 升冪(ascending) 排序
     .then(todos => res.render('index', { todos })) // 將 todos 資料，傳給前端 index 樣版
     .catch(error => console.error(error)) //如果發生意外，執行錯誤處理
 })
